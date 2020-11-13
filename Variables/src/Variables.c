d361c807f46725d6ca887494a187aad188e2b1b8
DESKTOP-JQ7HNAN\Alberto <acosta@itisrossi.vi.it>
2020-11-13 11:25:46 +0100

    Programma Variabili e Qualificatori

---
+++ Variables/src/Variables.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : Variables.c
+ Author      : Alberto Costa
+ Description : Le varibili primitive nel linguaggio C
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	/*
+	 TIPI DI VARIABILI
+	*/
+
+	// variabile intera --> 32 bit di memoria
+	int number = 0;
+	// variabile carattere
+	char character = 'a';
+	// variabile float
+	float decimalNumber = 1.2;
+	// variabile double
+	double anotherDecimanNumber = 2.3;
+
+	// stampo in console le variabili
+
+	printf("La variabile number vale: %d\n", number);
+	printf("La variabile character vale: %c\n", character);
+	printf("La variabile number vale: %f\n", decimalNumber);
+	printf("La variabile number vale: %f\n", anotherDecimanNumber);
+
+	/*
+	 QUALIFICATORI
+	*/
+
+
+}
